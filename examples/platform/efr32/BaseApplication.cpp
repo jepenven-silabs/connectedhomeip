ec66b2b7fc2a0d77c77d36e5402cc8b6a790bf22
jepenven-silabs <jean-francois.penven@silabs.com>
2022-08-19 19:35:48 -0400

    HotFix EFR32 pigweed RPC

--- examples/platform/efr32/BaseApplication.cpp
+++ examples/platform/efr32/BaseApplication.cpp
@@ -390,9 +390,10 @@ void BaseApplication::ButtonHandler(AppEvent * aEvent)
         {
             CancelFunctionTimer();
             mFunction = kFunction_NoneSelected;
-
+#ifdef DISPLAY_ENABLED
             // TOGGLE QRCode/LCD demo UI
             slLCD.ToggleQRCode();
+#endif
 
 #ifdef SL_WIFI
             if (!ConnectivityMgr().IsWiFiStationProvisioned())
